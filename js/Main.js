775f004d76ce74352b1850cfbcab1ffc071a3644
AndySimph <andytsimphaly@gmail.com>
2021-11-09 03:30:03 -0600

    Files

---
+++ js/Main.js
@@ -0,0 +1,15 @@
+
+new TypeIt("#myElement", {
+   
+    speed: 100,
+    deleteSpeed: 80,
+    waitUntilVisible: true,
+    loop: true,
+    cursor: true,
+    cursorSpeed: 1000,
+    cursorChar: "|",
+    startDelay: 300,
+    nextStringDelay: 1500,
+    loopDelay: 5000,
+    html: true,
+}).go();
